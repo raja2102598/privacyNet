4e592c97762f4a6a1d1e50bce40a4ad734f087b5
Raja Jaisankar <rr2102598@gmail.com>
2021-03-07 19:43:42 +0530

    db update

--- database/kinder.sql
+++ database/kinder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2021 at 05:52 PM
+-- Generation Time: Mar 07, 2021 at 03:13 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -44,8 +44,8 @@ CREATE TABLE `friends` (
 CREATE TABLE `login` (
   `login_id` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `mail_id` varchar(50) NOT NULL
+  `password` text NOT NULL,
+  `mail_id` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -53,9 +53,9 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`login_id`, `username`, `password`, `mail_id`) VALUES
-('22lqc8huvn', '', 'raja7252@', 'rr21025@gmail.com'),
-('hmieqdkbfx', '', 'raja7252@', 'rr210259@gmail.com'),
-('nbnuhpz7lg', '', 'raja7252', 'rr2102598@gmail.com');
+('glpuu696d6', '', 'U2FsdGVkX1/qaWFeMCLgbZFcwtcMY5WdFKuwMwdTuhQ=', 'test@gmail.com'),
+('jsnpeo81gp', '', 'U2FsdGVkX1+UCZ1p8BtSS7MBeXGKVlk4fcGb6aCVwrc=', 'rr2102598@gmail.com'),
+('z0bed6qdh3', '', 'U2FsdGVkX1818pJHZ0u/+wnHpsfgdI3iN0VnmWw1XlM=', 'rr210259@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -112,10 +112,10 @@ CREATE TABLE `users` (
   `u_lname` varchar(100) NOT NULL,
   `u_age` int(50) NOT NULL,
   `u_dob` date DEFAULT NULL,
-  `u_gender` varchar(20) NOT NULL,
+  `u_gender` text NOT NULL,
   `u_mobile` varchar(40) NOT NULL,
-  `u_email` varchar(40) NOT NULL,
-  `u_city` varchar(40) NOT NULL,
+  `u_email` longtext NOT NULL,
+  `u_city` text NOT NULL,
   `u_status` varchar(20) NOT NULL,
   `u_lastlogin` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
   `u_createdAt` timestamp(6) NULL DEFAULT NULL
@@ -126,9 +126,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_id`, `u_name`, `u_fname`, `u_lname`, `u_age`, `u_dob`, `u_gender`, `u_mobile`, `u_email`, `u_city`, `u_status`, `u_lastlogin`, `u_createdAt`) VALUES
-('22lqc8huvn', 'raja jv', 'raja', 'jv', 0, NULL, '', '', 'rr21025@gmail.com', '', 'true', '2021-03-05 16:16:23.380153', '2021-03-05 16:16:23.000000'),
-('hmieqdkbfx', 'rahaja kjjja', 'rahaja', 'kjjja', 0, NULL, '', '', 'rr210259@gmail.com', '', 'true', '2021-03-05 16:16:54.311159', '2021-03-05 16:16:54.000000'),
-('nbnuhpz7lg', 'Raja Jaisankar', 'Raja', 'Jaisankar', 0, NULL, '', '', 'rr2102598@gmail.com', '', 'true', '2021-03-05 16:03:32.223455', '2021-03-05 16:03:32.000000');
+('glpuu696d6', 'Raja Jaisankar', 'Raja', 'Jaisankar', 20, '2000-05-02', 'U2FsdGVkX1/b426GR+8D23Z31Uwd6hU4ThHUg85bDT8=', '', 'test@gmail.com', 'U2FsdGVkX18fSwLacEq1qASmzbqHadEj3bmJ7Xv1OGM=', 'true', '2021-03-06 16:20:33.328784', '2021-03-06 16:20:33.000000'),
+('jsnpeo81gp', 'Raja Jaisankar', 'Raja', 'Jaisankar', 20, '2000-05-02', 'U2FsdGVkX19tUem2egFrJTJnkMasbu7IbwrEif79xCY=', '', 'rr2102598@gmail.com', 'U2FsdGVkX19ZpYgi+LcjtF+olr4qPFXN3Jh9LNY1CsE=', 'true', '2021-03-06 10:27:02.683941', '2021-03-06 10:27:02.000000'),
+('z0bed6qdh3', 'raja Ja', 'raja', 'Ja', 0, NULL, '', '', 'rr210259@gmail.com', '', 'true', '2021-03-06 10:34:51.581766', '2021-03-06 10:34:51.000000');
 
 -- --------------------------------------------------------
 
@@ -139,14 +139,22 @@ INSERT INTO `users` (`u_id`, `u_name`, `u_fname`, `u_lname`, `u_age`, `u_dob`, `
 CREATE TABLE `user_profile` (
   `user_id` varchar(100) NOT NULL,
   `u_name` varchar(50) NOT NULL,
-  `u_gender` varchar(40) NOT NULL,
+  `u_gender` text NOT NULL,
   `u_dob` date NOT NULL,
-  `u_city` varchar(40) NOT NULL,
-  `u_hobby` varchar(40) NOT NULL,
-  `u_bio` varchar(100) NOT NULL,
-  `u_interests` varchar(50) NOT NULL,
+  `u_city` text NOT NULL,
+  `u_hobby` text NOT NULL,
+  `u_bio` text NOT NULL,
+  `u_interests` text NOT NULL,
   `u_email` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+INSERT INTO `user_profile` (`user_id`, `u_name`, `u_gender`, `u_dob`, `u_city`, `u_hobby`, `u_bio`, `u_interests`, `u_email`) VALUES
+('glpuu696d6', 'Raja Jaisankar', 'U2FsdGVkX1/b426GR+8D23Z31Uwd6hU4ThHUg85bDT8=', '2000-05-02', 'U2FsdGVkX18fSwLacEq1qASmzbqHadEj3bmJ7Xv1OGM=', 'U2FsdGVkX1+y5yI/dLPovoa8+hhO0Owvcbwy8sfQdr9YSzIN34/x2J7JVn3TH96XnCOV5VcycXBiD7H2h0B8DA==', 'U2FsdGVkX18wMWwQ/w3arp5SiBcQ7Cw0h7W1Iwwh72H+iQCqMjeIfZ/oJnlA4PSx', 'U2FsdGVkX18oLoaRecA9lJqlWMgPw1GIJMY9XaN6B5k=', 'test@gmail.com'),
+('jsnpeo81gp', 'Raja Jaisankar', 'U2FsdGVkX19tUem2egFrJTJnkMasbu7IbwrEif79xCY=', '2000-05-02', 'U2FsdGVkX19ZpYgi+LcjtF+olr4qPFXN3Jh9LNY1CsE=', 'U2FsdGVkX19I7gk1dlE+4uOLau8bihBMWgbLo1qkUYkAoKFuSqVPPq0rFpdCmgb9WrrjeMSXLQYbMn/0cxqeOQ==', 'U2FsdGVkX1/ZkOKGkbLG/GpcxGnmDWbgMVfGnrcufuphK5W8c7IAoz0evdOP25Fg', 'U2FsdGVkX19thJX/+yi+6DEm9qhmXFuWu25OuIeML3E=', 'rr2102598@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -191,7 +199,7 @@ ALTER TABLE `story`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`u_id`),
-  ADD UNIQUE KEY `u_email` (`u_email`);
+  ADD UNIQUE KEY `u_email` (`u_email`) USING HASH;
 
 --
 -- Indexes for table `user_profile`
