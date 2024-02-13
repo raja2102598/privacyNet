907a2fdc5bff7ebb069f6b82063fa91c42fbf2f1
Raja Jaisankar <36251374+raja2102598@users.noreply.github.com>
2024-02-13 16:08:06 +0530

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,4 @@
 module.exports = {
-  // HOST: "remotemysql.com",
-  // USERNAME: "aHenGQelbb",
-  // PASSWORD: "KTyw3LhbaF",
-  // DATABASE: "aHenGQelbb",
   HOST: "localhost",
   USERNAME: "root",
   PASSWORD: "",
